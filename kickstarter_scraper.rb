bf6931d7afaa46adf6b5347e1db2cd61c234cb32
thomas dubiel <td1729@hotmail.com>
2020-01-23 17:11:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,11 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html') 
   
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {} 
+  
+  kickstarter.css("li.project grid_4").each do |project| 
+    
+  end
 end
 
 create_project_hash
