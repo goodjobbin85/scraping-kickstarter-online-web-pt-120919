8206fea455edc87d5fa10bc7b6d458b89a129a41
thomas dubiel <td1729@hotmail.com>
2020-01-23 16:56:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require "pry"
   Scraping notes 
   
   Instance: <li class="project grid_4"> 
-  title: <h2 class="bbcard_name"> strong a 
+  title: <h2 class="bbcard_name"> strong a (project.css("h2.bbcard_name strong a").text)
   image_link: <img alt="Photo-little" class="projectphoto-little">
   description: <p class="bbcard_blurb>
   location: <ul class="project-meta"> a data-location
