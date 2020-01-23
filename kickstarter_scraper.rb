b7394ea4da3d0f825beecd5abcab8a7b244b2c1f
thomas dubiel <td1729@hotmail.com>
2020-01-23 16:44:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,26 @@
 # require libraries/modules here
+require "nokogiri"
+require "pry"
+
+=begin 
+  Scraping notes 
+  
+  Instance: <li class="project grid_4"> 
+  title: <h2 class="bbcard_name">
+  image_link: <img alt="Photo-little" class="projectphoto-little">
+  description: <p class="bbcard_blurb>
+  location: <ul class="project-meta"> a data-location
+  percent_funded: <li class="first funded">
+=end
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html') 
+  
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
+
+
